d1342ab1cdd9c5ee715e05fa78d9265b0031b992
Alexandre Gaspar <alexandretiao16@gmail.com>
2023-10-28 18:12:28 +0100

    Versão 1.0 do projecto gerador de minhas redes sociais

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn_mode = document.querySelector('#switch');
+btn_mode.onclick = alter_mode;
+
+//Função que altera do modo claro para escuro e vice-versa
+function alter_mode()
+{
+  let img_light = document.querySelector('#profile img');
+  let light_mode = document.querySelector('html');
+  light_mode.classList.toggle('light');
+  
+  //Analisa se está em light mode, se sim, altera a img e senão mantém
+  if (light_mode.classList.contains('light'))
+  {
+    img_light.setAttribute('src', './assets/avatar-light.png');
+  } else {
+    img_light.setAttribute('src', './assets/avatar.png');
+  }
+}
